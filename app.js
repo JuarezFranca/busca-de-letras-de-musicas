2f92cc635114aa9abaf95f0ac2c0e2b6fd0f5346
unknown <kjootaa@gmail.com>
2020-06-04 16:55:33 -0300

    implementação dos botoes Prox e ant

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 const apiURL = `https://api.lyrics.ovh/`
 
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await response.json()
+    insertSongsIntoPage(data)
+
+}
+
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => `
     <li class="song">
@@ -11,6 +18,15 @@ const insertSongsIntoPage = songsInfo => {
         <button class="btn" data-artist"${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
     </li>
     `).join('')
+    
+    if (songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+        ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Anteriores</button>` : ''}
+        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}`
+        return
+    }
+
+    prevAndNextContainer.innerHTML = ''
 }
 
 const fetchsongsterm = async term => {
