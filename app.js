49b5116e7518a79dd4df0f0e7ae4f05e72ea31cd
unknown <kjootaa@gmail.com>
2020-06-04 18:40:28 -0300

    inserindo funções ver letras

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+//consts que armazenam tags html inseridas no index como tambem o endereço http da api
 const form = document.querySelector('#form')
 const searchInput = document.querySelector('#search')
 const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 const apiURL = `https://api.lyrics.ovh/`
 
+//function responsavel por trazer as informações da API
 const getMoreSongs = async url => {
     const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
     const data = await response.json()
@@ -11,6 +13,7 @@ const getMoreSongs = async url => {
 
 }
 
+// mostra na pagina o nome da musica e nome do artista linkando com o cod HTML
 const insertSongsIntoPage = songsInfo => {
     songsContainer.innerHTML = songsInfo.data.map(song => `
     <li class="song">
@@ -19,10 +22,12 @@ const insertSongsIntoPage = songsInfo => {
     </li>
     `).join('')
     
+    // insire os botoes e invoca suas determinadas funções 
     if (songsInfo.prev || songsInfo.next){
         prevAndNextContainer.innerHTML = `
         ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Anteriores</button>` : ''}
-        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}`
+        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
         return
     }
 
@@ -41,13 +46,27 @@ form.addEventListener('submit', event => { // executa uma ação quando o formul
     event.preventDefault() // evita o envio do formulario
 
     const searchTerm = searchInput.value.trim() // trim; remove caracteres em branco antes ou depois
-
+    
+    // function que avalia se o usuario inseriu um termo valido
     if (!searchTerm) {
         songsContainer.innerHTML = `<li class="warning-message">por favor, digite um termo válido</li>`
         return
     }
 
     fetchsongsterm(searchTerm)
+})
 
-    
+const fetchLyrics = async (artist, songTitle) => {
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+    const data = await response.json()  
+}
+
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+
+    if (clickedElement.tagname === 'BUTTON') {
+        const artist = clickedElement.getAttribute('data-artist')
+        const songTitle = clickedElement.getAttribute('data-song-title')
+        fetchLyrics(artist, songTitle)
+    }
 })
